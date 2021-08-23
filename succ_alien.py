c7b5ca17337ecdf93671a7ff72ece735d42fd888
yrus <shahrushi199891@gmail.com>
2021-08-23 18:32:39 +0530

    Modified program to work with any base number system based on length of B.

--- succ_alien.py
+++ succ_alien.py
@@ -3,6 +3,8 @@ def create_dict_B_to_digit(B):
 	dict_B_to_digit = dict()
 	value = 0
 	for char in B:
+		if char in dict_B_to_digit:
+			raise Exception("Input Error: Duplicate characters in character set B !")
 		dict_B_to_digit[char] = value
 		value += 1
 	return dict_B_to_digit
@@ -22,40 +24,44 @@ def decode_alien_string(n, dict_B_to_digit):
 			raise Exception("Input Error: Number n contains characters not defined in the character set B!")
 	return num
 
-def convert_to_base10(num5):
+def convert_to_base10(num, base):
 	num10 = 0
 	power = 0
 
-	while num5!=0:
-		num10 += (num5%10) * (5 ** power)
+	while num!=0:
+		num10 += (num%10) * (base ** power)
 		power +=1
-		num5 = num5//10
+		num = num//10
 	
 	return num10
 
-def convert_to_base5_string(num10):
-	num5 = ""
+def convert_to_base_B_string(num10, base):
+	numB = ""
 	while num10!=0:
-		num5 = str(num10%5) + num5
-		num10 = num10//5
-	return num5
+		numB = str(num10%base) + numB
+		num10 = num10//base
+	return numB
 
 def succ_alien(n, B):
 	dict_B_to_digit = create_dict_B_to_digit(B)
 
-	num5 = decode_alien_string(n, dict_B_to_digit)
+	base = len(B)
 
-	num10 = convert_to_base10(num5)
+	numB = decode_alien_string(n, dict_B_to_digit)
+
+	num10 = convert_to_base10(numB, base)
 	# Increment
 	num10 += 1
 	
-	num5 = convert_to_base5_string(num10)
+	numB = convert_to_base_B_string(num10, base)
 
-	succ = encode_to_alien_string(num5, len(n)-len(num5), B)
+	succ = encode_to_alien_string(numB, len(n)-len(numB), B)
 
 	return succ
 
 
 if __name__ == "__main__":
 
-	print("n='!@^&*', B='!@^&*'' succ_alien:",succ_alien("!@^&*", "!@^&*"))
+	print("n='!@^&*', B='!@^&*' succ_alien:",succ_alien("!@^&*", "!@^&*"))
+	print("n='!!!***', B='!@^&*' succ_alien:",succ_alien("!!!***", "!@^&*"))
+	print("n='!@^&*#', B='!@^&*#' succ_alien:",succ_alien("!@^&*#", "!@^&*#"))
