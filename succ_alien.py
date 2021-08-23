1ae8b7bf345d034738a2cc8ed0eda3533d948c1c
yrus <shahrushi199891@gmail.com>
2021-08-23 16:57:01 +0530

    Decoupled functions to convert number among bases convert_to_base10(): Will convert given base 5 number to base 10 convert_to_base5_string(): Will convert given base 10 number to a base 5 string

--- succ_alien.py
+++ succ_alien.py
@@ -1,6 +1,5 @@
 
-
-def succ_alien(n, B):
+def convert_to_base10(n, B):
 	num10 = 0
 	power = 0
 	for ni in range(len(n)-1, -1, -1):
@@ -9,14 +8,27 @@ def succ_alien(n, B):
 				num10 += ci * (5 ** power)
 				power +=1
 				break
+	return num10
 
-	num10 += 1
-	# print(num10)
+def convert_to_base5_string(num10):
 	num5 = ""
 	while num10!=0:
 		num5 = str(num10%5) + num5
 		num10 = num10//5
+	return num5
+
+def succ_alien(n, B):
+	
+
+	num10 = convert_to_base10(n, B)
+	# Increment
+	num10 += 1
+
+	# print(num10)
+	
+	num5 = convert_to_base5_string(num10)
 	# print(num5)
+	
 	succ = B[0] * (len(n) - len(num5))
 	for ni in num5:
 		succ += B[int(ni)]
